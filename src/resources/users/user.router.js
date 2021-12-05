cef313b8eb072d3cc5a53d8dd9aea86a96f53856
Yauheni <gerasik1992@gmail.com>
2021-12-05 22:06:21 +0300

    user api

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -1,11 +1,84 @@
-const router = require('express').Router();
-const User = require('./user.model');
-const usersService = require('./user.service');
+const { getAll, getOne, create, update, remove } = require('./user.service');
+const { User } = require('./user.model');
 
-router.route('/').get(async (req, res) => {
-  const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
-});
+const mainUrl = '/users';
 
-module.exports = router;
+const routes = [
+  {
+    method: 'GET',
+    url: mainUrl,
+    handler: getAll,
+    schema: {
+      response: {
+        200: {
+          type: 'array',
+          users: User,
+        },
+      },
+    },
+  },
+  {
+    method: 'GET',
+    url: `${mainUrl}/:userId`,
+    handler: getOne,
+    schema: {
+      response: {
+        200: User,
+      },
+    },
+  },
+  {
+    method: 'POST',
+    url: mainUrl,
+    handler: create,
+    schema: {
+      body: {
+        type: 'object',
+        required: ['name', 'login', 'password'],
+        properties: {
+          name: { type: 'string' },
+          login: { type: 'string' },
+          password: { type: 'string' },
+        },
+      },
+      response: {
+        201: User,
+      },
+    },
+  },
+  {
+    method: 'PUT',
+    url: `${mainUrl}/:userId`,
+    handler: update,
+    schema: {
+      body: {
+        type: 'object',
+        properties: {
+          name: { type: 'string' },
+          login: { type: 'string' },
+          password: { type: 'string' },
+        },
+      },
+      response: {
+        200: User,
+      },
+    },
+  },
+  {
+    method: 'DELETE',
+    url: `${mainUrl}/:userId`,
+    handler: remove,
+    schema: {
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            message: { type: 'string' },
+          },
+        },
+      },
+    },
+  },
+];
+
+module.exports = routes;
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,53 @@
-const usersRepo = require('./user.memory.repository');
+const { validate } = require('uuid');
 
-const getAll = () => usersRepo.getAll();
+const userRepository = require('./user.memory.repository');
 
-module.exports = { getAll };
+exports.getAll = () => userRepository.getAll();
+
+exports.getOne = (req, reply) => {
+  const { userId } = req.params;
+
+  if (!validate(userId)) {
+    return reply.code(400).send({ message: 'User id is not valid' });
+  }
+
+  const isRemove = userRepository.getOne(userId);
+
+  if (!isRemove) {
+    reply.code(404).send({ message: `user with id: ${userId} did not found` });
+  }
+  return isRemove;
+};
+
+exports.create = (req, reply) => {
+  reply.code(201).send(userRepository.create(req.body));
+};
+
+exports.update = (req, reply) => {
+  console.log(req);
+  const { userId } = req.params;
+  if (!validate(userId)) {
+    return reply.code(400).send({ message: 'User id is not valid' });
+  }
+
+  const isUpdated = userRepository.update(userId, req.body);
+
+  if (!isUpdated) {
+    reply.code(404).send({ message: `user with id: ${userId} did not found` });
+  }
+  return isUpdated;
+};
+
+exports.remove = (req, reply) => {
+  const { userId } = req.params;
+  if (!validate(userId)) {
+    return reply.code(400).send({ message: 'User id is not valid' });
+  }
+
+  const isRemove = userRepository.remove(userId);
+
+  if (!isRemove) {
+    reply.code(404).send({ message: `user with id: ${userId} did not found` });
+  }
+  return isRemove;
+};
