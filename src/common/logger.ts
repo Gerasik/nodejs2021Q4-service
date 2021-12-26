dbb9e6fd587923b48d7babb29b719e887f1ab1ba
Yauheni <gerasik1992@gmail.com>
2021-12-26 23:50:08 +0300

    Update config

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -3,36 +3,44 @@ import { TransportMultiOptions, pino } from 'pino';
 
 import config from './config';
 
+const LOG_FOLDER = 'logs';
+const ALL_LOGS_FILE_NAME = 'all';
+const ERROR_LOGS_FILE_NAME = 'error';
+
+const serializers = {
+  res(reply: FastifyReply) {
+    return {
+      statusCode: reply.statusCode,
+    };
+  },
+  req(request: FastifyRequest) {
+    return {
+      method: request.method,
+      url: request.url,
+      path: request.routerPath,
+      parameters: request.params,
+      query: request.query,
+    };
+  },
+};
+
+const targets = [
+  {
+    level: 'trace',
+    target: 'pino/file',
+    options: { destination: `./${LOG_FOLDER}/${ALL_LOGS_FILE_NAME}.json` },
+  },
+  {
+    level: 'error',
+    target: 'pino/file',
+    options: { destination: `./${LOG_FOLDER}/${ERROR_LOGS_FILE_NAME}.json` },
+  },
+];
+
 const transportConf = pino.transport(<TransportMultiOptions>{
   level: config.LOG_LEVEL,
-  serializers: {
-    res(reply: FastifyReply) {
-      return {
-        statusCode: reply.statusCode,
-      };
-    },
-    req(request: FastifyRequest) {
-      return {
-        method: request.method,
-        url: request.url,
-        path: request.routerPath,
-        parameters: request.params,
-        query: request.query,
-      };
-    },
-  },
-  targets: [
-    {
-      level: 'trace',
-      target: 'pino/file',
-      options: { destination: './logs/all.json' },
-    },
-    {
-      level: 'error',
-      target: 'pino/file',
-      options: { destination: './logs/error.json' },
-    },
-  ],
+  serializers,
+  targets,
 });
 
 const logger = pino(transportConf);
