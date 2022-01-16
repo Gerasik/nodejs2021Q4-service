b2f5fcd1302d230f08056d3db031f75b6e52fed7
Yauheni <gerasik1992@gmail.com>
2022-01-16 22:55:16 +0300

    config database

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import config from './common/config';
 import app from './app';
+import connectPostrgess from './db/db';
 
 const HOST = config.HOST as string;
 const PORT = config.PORT as string;
 
 const start = async () => {
+  await connectPostrgess();
+
   try {
     console.log(`App is running on http://${HOST}:${PORT}`);
     await app.listen(PORT, HOST);
