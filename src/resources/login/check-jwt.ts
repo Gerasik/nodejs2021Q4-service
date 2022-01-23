485ecd1e6df1ba199aa8fdd0b53a8b8db3039ff8
Yauheni <gerasik1992@gmail.com>
2022-01-23 23:32:52 +0300

    token fix

--- src/resources/login/check-jwt.ts
+++ src/resources/login/check-jwt.ts
@@ -1,19 +1,56 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
+import jwt from 'jsonwebtoken';
 import config from '../../common/config';
+import { getOne, getUserByLogin } from '../users/user.memory.repository';
 
 const checkJWT = async (req: FastifyRequest, reply: FastifyReply) => {
   const authHeader = req.headers.authorization;
 
-  const splitted = authHeader ? authHeader.split(' ') : null;
+  if (!authHeader) {
+    reply.code(401).send({ message: `Unauthorized error` });
+  } else {
+    const splittedHeader = authHeader.split(' ');
 
-  if (authHeader && splitted && splitted[0] === 'Bearer' && splitted[1]) {
-    const token = splitted[1];
+    if (
+      !(
+        authHeader &&
+        splittedHeader &&
+        splittedHeader[0] === 'Bearer' &&
+        splittedHeader[1]
+      )
+    ) {
+      reply.code(401).send({ message: `Unauthorized error` });
+    }
 
-    console.log(`Token: ${token}`);
+    const token = splittedHeader[1];
+
+    const tokenPayload = await new Promise((resolve, reject) => {
+      jwt.verify(token, config.JWT_SECRET_KEY, (err, payload) => {
+        if (err) reject(err);
+
+        resolve(payload);
+      });
+    }).catch((e) => {
+      reply.code(401).send({ message: e.message });
+    });
+
+    const { userId, login } = tokenPayload as { userId: string; login: string };
+
+    const userById = await getOne(userId);
+    if (!userById) {
+      reply
+        .code(401)
+        .send({ message: `Userid defined in Authorization token not found` });
+    }
+
+    const userByLogin = await getUserByLogin(login as string);
+    if (!userByLogin) {
+      reply.code(401).send({
+        message: `Username defined in the authorization token not found`,
+      });
+    }
 
     return { req, reply };
   }
-
-  reply.code(401).send({ message: `Unauthorized error` });
 };
 export default checkJWT;
